bebd8d819f288a8a7093470cce590cc4671321e0
Jacob <jacobm3@gmail.com>
2021-09-18 15:15:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,23 @@ provider "aws" {
  }
 }
 
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-074a9976b3234465b"
+  ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t3.nano"
 }
 
