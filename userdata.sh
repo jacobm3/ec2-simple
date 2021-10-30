80a23faeb5459695597033ef9f9664591edc46eb
Jacob <jacobm3@gmail.com>
2021-10-29 20:53:23 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,3 @@
 curl -fsSL https://raw.githubusercontent.com/jacobm3/gbin/main/ubuntu-setup.sh | bash
 
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/ACtnidIBcfq5Mp41uxDz3dL8ONLrGOwFts/c24acc jacob@acer" >> /home/ubuntu/.ssh/authorized_keys
